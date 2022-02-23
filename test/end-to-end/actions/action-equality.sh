68f3bfc6989b7ad8ca40e18538ff15fb7b72e33a
Klaus Aehlig <klaus.aehlig@huawei.com>
2022-02-23 15:10:43 +0100

    Add a first end-to-end test for action equality
    This test also demonstrates the notion of equality used in our
    action graph: actions are considered equal, if they are defined in
    the same way (regardless of where they are defined); when looking up
    actions in cache, however, the inputs are considered extensionally.
    The test also verifies that if one dumps the action graph, the
    origins of an action (as the same action can be defined in many
    places) are reported correctly.

---
+++ test/end-to-end/actions/action-equality.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -e
+
+mkdir .tool-root
+touch ROOT
+cat > TARGETS <<'EOI'
+{ "foo":
+  { "type": "generic"
+  , "outs": ["out.txt"]
+  , "cmds": ["echo Hello World > out.txt"]
+  }
+, "bar":
+  { "type": "generic"
+  , "outs": ["out.txt"]
+  , "cmds": ["echo Hello World > out.txt"]
+  }
+, "baz":
+  { "type": "generic"
+  , "outs": ["out.txt"]
+  , "cmds": ["echo -n Hello > out.txt && echo ' World' >> out.txt"]
+  }
+, "foo upper":
+  { "type": "generic"
+  , "deps": ["foo"]
+  , "outs": ["upper.txt"]
+  , "cmds": ["cat out.txt | tr a-z A-Z > upper.txt"]
+  }
+, "bar upper":
+  { "type": "generic"
+  , "deps": ["bar"]
+  , "outs": ["upper.txt"]
+  , "cmds": ["cat out.txt | tr a-z A-Z > upper.txt"]
+  }
+, "baz upper":
+  { "type": "generic"
+  , "deps": ["baz"]
+  , "outs": ["upper.txt"]
+  , "cmds": ["cat out.txt | tr a-z A-Z > upper.txt"]
+  }
+, "ALL":
+  { "type": "install"
+  , "files":
+    {"foo.txt": "foo upper", "bar.txt": "bar upper", "baz.txt": "baz upper"}
+  }
+}
+EOI
+
+
+bin/tool-under-test build -J 1 --local_build_root .tool-root 2>log
+cat log
+echo
+grep '4 actions' log
+grep '1 cache hit' log
+
+echo
+bin/tool-under-test analyse --dump_graph graph.json
+echo
+matching_targets=$(cat graph.json | jq -acM '.actions | [ .[] | .origins | [ .[] | .target]] | sort')
+echo "${matching_targets}"
+[ "${matching_targets}" = '[[["@","","","bar"],["@","","","foo"]],[["@","","","bar upper"],["@","","","foo upper"]],[["@","","","baz"]],[["@","","","baz upper"]]]' ]
