7ff771321861559905f1681eae8c48b5ef48705f
Klaus Aehlig <klaus.aehlig@huawei.com>
2022-03-02 18:09:07 +0100

    Add end-to-end test using built shell scripts to generate files

---
+++ test/end-to-end/generated-binary/shell-script.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -e
+
+mkdir .tool-root
+touch ROOT
+cat > TARGETS <<'EOI'
+{ "script-hello":
+  { "type": "generic"
+  , "outs": ["script.sh"]
+  , "cmds":
+    [ "echo '#!/bin/sh' > script.sh"
+    , "echo 'echo Hello World' >> script.sh"
+    , "chmod 755 script.sh"
+    ]
+  }
+, "generated-hello":
+  { "type": "generic"
+  , "outs": ["out-hello.txt"]
+  , "deps": ["script-hello"]
+  , "cmds": ["./script.sh > out-hello.txt"]
+  }
+, "script-morning":
+  { "type": "generic"
+  , "outs": ["script.sh"]
+  , "cmds":
+    [ "echo '#!/bin/sh' > script.sh"
+    , "echo 'echo Good morning' >> script.sh"
+    , "chmod 755 script.sh"
+    ]
+  }
+, "generated-morning":
+  { "type": "generic"
+  , "outs": ["out-morning.txt"]
+  , "deps": ["script-morning"]
+  , "cmds": ["./script.sh > out-morning.txt"]
+  }
+, "ALL":
+  {"type": "generic"
+  , "deps": ["generated-hello", "generated-morning"]
+  , "outs": ["out.txt"]
+  , "cmds": ["cat out-*.txt > out.txt"]
+  }
+}
+EOI
+
+
+bin/tool-under-test install -o out --local_build_root .tool-root 2>&1
+grep Hello out/out.txt
+grep Good out/out.txt
