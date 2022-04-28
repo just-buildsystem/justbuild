05ec4cd875123813ce3822bfea34f03cf3514bde
Alberto Sartori <alberto.sartori@huawei.com>
2022-04-28 13:15:46 +0200

    add simple bash completion for targets

--- share/just_complete.bash
+++ share/just_complete.bash
@@ -6,9 +6,37 @@ _just_subcommand_options(){
     done
 }
 
+_just_targets(){
+    command -v python3 &>/dev/null || return
+    python3 - <<EOF
+from json import load
+from os import path
+
+def print_targets(target_file):
+    if not path.exists(target_file):
+        return
+    with open(target_file) as f:
+        targets = load(f)
+        for t in targets.keys():
+            print(t)
+    exit()
+
+def main(prev, name):
+    # if prev is a directory, then look for targets there
+    if path.isdir(prev):
+        print_targets(path.join(prev, name))
+
+    # fall back to current directory
+    print_targets(name)
+
+main('$1', "TARGETS")
+EOF
+}
+
 _just_completion(){
     local readonly SUBCOMMANDS=(build analyse describe install-cas install rebuild -h --help)
     local word=${COMP_WORDS[$COMP_CWORD]}
+    local prev=${COMP_WORDS[$((COMP_CWORD-1))]}
     local cmd=${COMP_WORDS[1]}
     # first check if the current word matches a subcommand
     # if we check directly with cmd, we fail to autocomplete install to install-cas
@@ -17,8 +45,10 @@ _just_completion(){
         COMPREPLY=($(compgen -W "${SUBCOMMANDS[*]}" -- $word))
     elif [[ $cmd =~ ^(build|analyse|describe|install-cas|install|rebuild) ]]
     then
-        COMPREPLY=($(compgen -f -W "$(_just_subcommand_options $cmd)" -- $word ))
-        compopt -o plusdirs -o bashdefault
+        local _opts=($(_just_subcommand_options $cmd))
+        local _targets=($(_just_targets $prev))
+        COMPREPLY=($(compgen -f -W "${_opts[*]} ${_targets[*]}" -- $word ))
+        compopt -o plusdirs -o bashdefault -o default
     else
         COMPREPLY=($(compgen -W "${SUBCOMMANDS[*]}" -- $word))
     fi
