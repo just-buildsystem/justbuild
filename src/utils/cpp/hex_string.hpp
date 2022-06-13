f5c12e59e34107f1fc16349d843a8f64d7dd5459
Klaus Aehlig <klaus.aehlig@huawei.com>
2022-06-13 13:30:13 +0200

    utils: convert hex to string

--- src/utils/cpp/hex_string.hpp
+++ src/utils/cpp/hex_string.hpp
@@ -2,6 +2,8 @@
 #define INCLUDED_SRC_UTILS_CPP_HEX_STRING_HPP
 
 #include <iomanip>
+#include <iostream>
+#include <optional>
 #include <sstream>
 #include <string>
 
@@ -14,6 +16,22 @@
            << static_cast<int>(static_cast<unsigned char const>(b));
     }
     return ss.str();
+}
+
+[[nodiscard]] static inline auto FromHexString(std::string const& hexstring)
+    -> std::optional<std::string> {
+    try {
+        const size_t kHexBase = 16;
+        std::stringstream ss{};
+        for (size_t i = 0; i < hexstring.length(); i += 2) {
+            unsigned char c =
+                std::stoul(hexstring.substr(i, 2), nullptr, kHexBase);
+            ss << c;
+        }
+        return ss.str();
+    } catch (...) {
+        return std::nullopt;
+    }
 }
 
 #endif  // INCLUDED_SRC_UTILS_CPP_HEX_STRING_HPP
