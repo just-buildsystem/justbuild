f19c4d9296881083b8dc313e03c6535c68daa7a2
Klaus Aehlig <klaus.aehlig@huawei.com>
2022-08-19 14:30:57 +0200

    Bump verison to 1.0.0~beta2

--- src/buildtool/main/version.cpp
+++ src/buildtool/main/version.cpp
@@ -7,7 +7,7 @@ auto version() -> std::string {
     std::size_t major = 1;
     std::size_t minor = 0;
     std::size_t revision = 0;
-    std::string suffix = "~beta1";
+    std::string suffix = "~beta2";
 #ifdef VERSION_EXTRA_SUFFIX
     suffix += VERSION_EXTRA_SUFFIX;
 #endif
