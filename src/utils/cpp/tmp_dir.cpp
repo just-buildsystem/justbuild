f519864e0492af758108c48c1ddeb1356f1c03b3
Paul Cristian Sarbu <paul.cristian.sarbu@huawei.com>
2022-08-31 09:37:13 +0200

    Utils: Add class handling tmp directories

---
+++ src/utils/cpp/tmp_dir.cpp
@@ -0,0 +1,64 @@
+// Copyright 2022 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/utils/cpp/tmp_dir.hpp"
+
+#include "gsl-lite/gsl-lite.hpp"
+#include "src/buildtool/file_system/file_system_manager.hpp"
+
+auto TmpDir::Create(std::filesystem::path const& prefix,
+                    std::string const& dir_template) noexcept -> TmpDirPtr {
+    try {
+        // make sure prefix folder exists
+        if (not FileSystemManager::CreateDirectory(prefix)) {
+            Logger::Log(LogLevel::Error,
+                        "TmpDir: could not create prefix directory {}",
+                        prefix.string());
+            return nullptr;
+        }
+
+        auto full_template_str =
+            std::filesystem::weakly_canonical(
+                std::filesystem::absolute(prefix / dir_template))
+                .string();
+
+        std::vector<char> c_tmpl(full_template_str.begin(),
+                                 full_template_str.end());
+        c_tmpl.emplace_back('\0');  // get as c-string
+
+        // attempt to make the tmp dir
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg,hicpp-vararg)
+        char* tmp_dir = mkdtemp(&c_tmpl[0]);
+        if (tmp_dir == nullptr) {
+            return nullptr;
+        }
+        auto tmp_dir_obj = std::make_shared<TmpDir>();
+        // need to explicitly transform to std::string first
+        tmp_dir_obj->tmp_dir_ = std::filesystem::path(std::string(tmp_dir));
+
+        return std::static_pointer_cast<TmpDir const>(tmp_dir_obj);
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+auto TmpDir::GetPath() const noexcept -> std::filesystem::path {
+    return tmp_dir_;
+}
+
+TmpDir::~TmpDir() noexcept {
+    // try to remove the tmp dir and all its content
+    [[maybe_unused]] auto res =
+        FileSystemManager::RemoveDirectory(tmp_dir_, true);
+}
---
+++ src/utils/cpp/tmp_dir.hpp
@@ -0,0 +1,54 @@
+// Copyright 2022 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_SRC_OTHER_TOOLS_TMP_DIR_HPP
+#define INCLUDED_SRC_OTHER_TOOLS_TMP_DIR_HPP
+
+#include <cstdlib>
+#include <filesystem>
+#include <optional>
+#include <string>
+
+std::string const kDefaultTemplate{"tmp.XXXXXX"};
+
+class TmpDir;
+using TmpDirPtr = std::shared_ptr<TmpDir const>;
+
+class TmpDir {
+  public:
+    // default ctor; not to be used!
+    TmpDir() = default;
+
+    /// \brief Destroy the TmpDir object. It tries to remove the tmp folder.
+    ~TmpDir() noexcept;
+
+    // no copies, only moves
+    TmpDir(TmpDir const&) = delete;
+    TmpDir(TmpDir&& other) noexcept = default;
+    auto operator=(TmpDir const&) = delete;
+    auto operator=(TmpDir&& other) noexcept -> TmpDir& = default;
+
+    [[nodiscard]] auto GetPath() const noexcept -> std::filesystem::path;
+
+    /// \brief Creates a completely unique directory in a given prefix path.
+    [[nodiscard]] static auto Create(
+        std::filesystem::path const& prefix,
+        std::string const& dir_template = kDefaultTemplate) noexcept
+        -> TmpDirPtr;
+
+  private:
+    std::filesystem::path tmp_dir_{};
+};
+
+#endif  // INCLUDED_SRC_OTHER_TOOLS_TMP_DIR_HPP
