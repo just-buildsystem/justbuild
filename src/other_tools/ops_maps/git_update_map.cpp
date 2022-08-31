f0f95b7cf06b3d4c2848a4715766a330d1d71d4f
Paul Cristian Sarbu <paul.cristian.sarbu@huawei.com>
2022-08-31 15:29:16 +0200

    Just-MR: Add git commit update map

---
+++ src/other_tools/ops_maps/git_update_map.cpp
@@ -0,0 +1,64 @@
+// Copyright 2022 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/other_tools/ops_maps/git_update_map.hpp"
+
+#include "fmt/core.h"
+#include "src/buildtool/execution_api/local/config.hpp"
+#include "src/other_tools/just_mr/utils.hpp"
+#include "src/utils/cpp/tmp_dir.hpp"
+
+auto CreateGitUpdateMap(GitCASPtr const& git_cas, std::size_t jobs)
+    -> GitUpdateMap {
+    auto update_commits = [git_cas](auto /* unused */,
+                                    auto setter,
+                                    auto logger,
+                                    auto /* unused */,
+                                    auto const& key) {
+        // perform git update commit
+        auto git_repo = GitRepo::Open(git_cas);  // wrap the tmp odb
+        if (not git_repo) {
+            (*logger)(
+                fmt::format("Failed to open tmp Git repository for remote {}",
+                            key.first),
+                /*fatal=*/true);
+            return;
+        }
+        auto tmp_dir = JustMR::Utils::CreateTypedTmpDir("update");
+        if (not tmp_dir) {
+            (*logger)(fmt::format("Failed to create commit update tmp dir for "
+                                  "remote {}",
+                                  key.first),
+                      /*fatal=*/true);
+            return;
+        }
+        // setup wrapped logger
+        auto wrapped_logger = std::make_shared<AsyncMapConsumerLogger>(
+            [&logger](auto const& msg, bool fatal) {
+                (*logger)(
+                    fmt::format("While updating commit via tmp repo:\n{}", msg),
+                    fatal);
+            });
+        // update commit
+        auto refname = std::string("refs/heads/") +
+                       key.second;  // assume branch ref format
+        auto new_commit = git_repo->UpdateCommitViaTmpRepo(
+            tmp_dir->GetPath(), key.first, refname, wrapped_logger);
+        if (not new_commit) {
+            return;
+        }
+        (*setter)(new_commit->c_str());
+    };
+    return AsyncMapConsumer<StringPair, std::string>(update_commits, jobs);
+}
---
+++ src/other_tools/ops_maps/git_update_map.hpp
@@ -0,0 +1,43 @@
+// Copyright 2022 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_SRC_OTHER_TOOLS_OPS_MAPS_GIT_UPDATE_MAP_HPP
+#define INCLUDED_SRC_OTHER_TOOLS_OPS_MAPS_GIT_UPDATE_MAP_HPP
+
+#include "src/buildtool/file_system/git_repo.hpp"
+#include "src/buildtool/multithreading/async_map_consumer.hpp"
+#include "src/utils/cpp/hash_combine.hpp"
+
+using StringPair = std::pair<std::string, std::string>;
+
+/// \brief Maps a pair of repository url and branch to an updated commit hash.
+using GitUpdateMap = AsyncMapConsumer<StringPair, std::string>;
+
+namespace std {
+template <>
+struct hash<StringPair> {
+    [[nodiscard]] auto operator()(StringPair const& ct) const noexcept
+        -> std::size_t {
+        size_t seed{};
+        hash_combine<std::string>(&seed, ct.first);
+        hash_combine<std::string>(&seed, ct.second);
+        return seed;
+    }
+};
+}  // namespace std
+
+[[nodiscard]] auto CreateGitUpdateMap(GitCASPtr const& git_cas,
+                                      std::size_t jobs) -> GitUpdateMap;
+
+#endif  // INCLUDED_SRC_OTHER_TOOLS_OPS_MAPS_GIT_UPDATE_MAP_HPP
