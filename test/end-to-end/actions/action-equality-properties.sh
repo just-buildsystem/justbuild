5b2f60377b591c9a5602cd913f880c93506f2b1b
Klaus Aehlig <klaus.aehlig@huawei.com>
2023-06-21 14:07:44 +0200

    Extend action-equality-properties test to also test the generic rule

--- test/end-to-end/actions/action-equality-properties.sh
+++ test/end-to-end/actions/action-equality-properties.sh
@@ -15,9 +15,12 @@
 
 set -e
 
+readonly ROOT="$(pwd)"
 readonly LBRDIR="$TMPDIR/local-build-root"
-readonly JUST="bin/tool-under-test"
+readonly JUST="${ROOT}/bin/tool-under-test"
 
+mkdir -p "${ROOT}/rules"
+cd "${ROOT}/rules"
 touch ROOT
 cat > RULES <<'EOI'
 { "":
@@ -27,7 +30,7 @@ cat > RULES <<'EOI'
     , "artifacts":
       { "type": "ACTION"
       , "outs": ["out.txt"]
-      , "cmd": ["sh", "-c", "echo Hello > out.txt"]
+      , "cmd": ["sh", "-c", "echo Hello > out.txt\n"]
       , "execution properties": {"type": "var", "name": "PROPERTIES"}
       }
     }
@@ -57,6 +60,47 @@ cat > TARGETS <<'EOI'
 }
 EOI
 
+mkdir -p "${ROOT}/generic"
+cd "${ROOT}/generic"
+touch ROOT
+
+cat > TARGETS <<'EOI'
+{ "flexible":
+  { "type": "generic"
+  , "arguments_config": ["PROPERTIES"]
+  , "outs": ["out.txt"]
+  , "cmds": ["echo Hello > out.txt"]
+  , "execution properties": {"type": "var", "name": "PROPERTIES"}
+  }
+, "fixed":
+  { "type": "configure"
+  , "target": "flexible"
+  , "config":
+    { "type": "singleton_map"
+    , "key": "PROPERTIES"
+    , "value": {"type": "empty_map"}
+    }
+  }
+, "unset":
+  { "type": "configure"
+  , "target": "flexible"
+  , "config": {"type": "singleton_map", "key": "PROPERTIES", "value": null}
+  }
+, "":
+  { "type": "install"
+  , "files": {"flexible": "flexible", "fixed": "fixed", "unset": "unset"}
+  }
+}
+EOI
+
+
+
+for variant in rules generic
+do
+echo "Testing variant ${variant}"
+echo
+cd "${ROOT}/${variant}"
+
 "${JUST}" analyse --local-build-root "${LBRDIR}" --dump-graph graph-null.json 2>&1
 action_configs=$(cat graph-null.json | jq -acM '.actions | [ .[] | .origins | [ .[] | .config.PROPERTIES ]] | sort')
 echo "${action_configs}"
@@ -72,5 +116,15 @@ echo "${action_configs}"
 action_configs=$(cat graph-set.json | jq -acM '.actions | [ .[] | .origins | [ .[] | .config.PROPERTIES ]] | sort')
 echo "${action_configs}"
 [ "${action_configs}" = '[[null,{}],[{"foo":"bar"}]]' ]
+
+echo "variant ${variant} OK"
+echo
+done
+
+# also verify the consistency of the generated actions
+for graph in graph-null.json graph-empty.json graph-set.json
+do
+    diff -u "${ROOT}/rules/${graph}" "${ROOT}/generic/${graph}"
+done
 
 echo OK
