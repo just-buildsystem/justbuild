82e0bd818aee1ca8751e0abf20455af88cc513bf
Oliver Reiche <oliver.reiche@huawei.com>
2023-08-29 14:20:58 +0200

    utils: No hash for upcoming libstdc++ 11 releases

--- src/utils/cpp/path_hash.hpp
+++ src/utils/cpp/path_hash.hpp
@@ -17,12 +17,13 @@
 
 #include <filesystem>
 
-auto constexpr kGLIBCXX_11_4 = 20230528;  // gcc/DATESTAMP of version 11.4
-#if (defined(__GLIBCXX__) and _GLIBCXX_RELEASE < 12 and \
-     __GLIBCXX__ != kGLIBCXX_11_4) or                   \
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define GLIBCXX_11_4 20230528  // gcc/DATESTAMP of version 11.4
+#if (defined(__GLIBCXX__) and _GLIBCXX_RELEASE < 12 and            \
+     !(_GLIBCXX_RELEASE == 11 and __GLIBCXX__ >= GLIBCXX_11_4)) or \
     (defined(_LIBCPP_VERSION) and _LIBCPP_VERSION < 16000)
 // std::hash<std::filesystem::path> is missing for
-// - GNU's libstdc++ < 12 (except 11.4)
+// - GNU's libstdc++ < 11.4
 // - LLVM's libcxx < 16 (see https://reviews.llvm.org/D125394)
 namespace std {
 template <>
