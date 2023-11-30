4c9295a45adab05571b8f81027f936522a6b48d3
Paul Cristian Sarbu <paul.cristian.sarbu@huawei.com>
2023-11-30 14:11:35 +0100

    git_repo_remote: Fix wrong header include format

--- src/other_tools/git_operations/git_repo_remote.cpp
+++ src/other_tools/git_operations/git_repo_remote.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <src/other_tools/git_operations/git_repo_remote.hpp>
+#include "src/other_tools/git_operations/git_repo_remote.hpp"
 
 #include "fmt/core.h"
 #include "nlohmann/json.hpp"
