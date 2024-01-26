60bcae364d394c0ac7a2fdb28b7905480be3a79c
Klaus Aehlig <klaus.aehlig@huawei.com>
2024-01-26 14:34:49 +0100

    Add library for overlaying just-mr rcs

---
+++ src/other_tools/just_mr/rc_merge.cpp
@@ -0,0 +1,71 @@
+// Copyright 2024 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/other_tools/just_mr/rc_merge.hpp"
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#include "src/buildtool/build_engine/expression/expression.hpp"
+
+namespace {
+
+auto const kAccumulating = std::vector<std::string>{"distdirs"};
+auto const kLocalMerge = std::vector<std::string>{"just args", "just files"};
+
+}  // namespace
+
+[[nodiscard]] auto MergeMRRC(const Configuration& base,
+                             const Configuration& delta) noexcept
+    -> Configuration {
+    // For most fields, just let the delta entry override
+    auto result = base.Update(delta.Expr());
+
+    // Accumulating fields
+    for (auto const& f : kAccumulating) {
+        auto joined = Expression::list_t{};
+        auto const& deltaf = delta[f];
+        if (deltaf->IsList()) {
+            std::copy(deltaf->List().begin(),
+                      deltaf->List().end(),
+                      std::back_inserter(joined));
+        }
+        auto const& basef = base[f];
+        if (basef->IsList()) {
+            std::copy(basef->List().begin(),
+                      basef->List().end(),
+                      std::back_inserter(joined));
+        }
+        result = result.Update(
+            ExpressionPtr{Expression::map_t{f, ExpressionPtr{joined}}});
+    }
+
+    // Locally-merging fields
+    for (auto const& f : kLocalMerge) {
+        auto joined = Configuration{Expression::kEmptyMap};
+        auto const& basef = base[f];
+        if (basef->IsMap()) {
+            joined = joined.Update(basef);
+        }
+        auto const& deltaf = delta[f];
+        if (deltaf->IsMap()) {
+            joined = joined.Update(deltaf);
+        }
+        result =
+            result.Update(ExpressionPtr{Expression::map_t{f, joined.Expr()}});
+    }
+
+    return result;
+}
---
+++ test/other_tools/just_mr/rc_merge.test.cpp
@@ -0,0 +1,67 @@
+// Copyright 2024 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+
+#include "catch2/catch_test_macros.hpp"
+#include "src/other_tools/just_mr/rc_merge.hpp"
+
+TEST_CASE("Simple field") {
+    auto conf = Configuration{Expression::FromJson(R"(
+         { "log limit": 4
+         , "git": {"root": "system", "path": "usr/bin/git"}
+         })"_json)};
+    auto delta = Configuration{Expression::FromJson(R"(
+         { "log limit": 5
+         })"_json)};
+
+    auto merged = MergeMRRC(conf, delta);
+    CHECK(merged["log limit"] == Expression::FromJson("5"_json));
+    CHECK(merged["git"] ==
+          Expression::FromJson(
+              R"({"root": "system", "path": "usr/bin/git"})"_json));
+}
+
+TEST_CASE("accumulating") {
+    auto conf = Configuration{Expression::FromJson(R"(
+      {"distdirs": [{"root": "home", "path": ".distfiles"}]}
+    )"_json)};
+    auto delta = Configuration{Expression::FromJson(R"(
+      {"distdirs": [{"root": "workspace", "path": "third_party"}]}
+    )"_json)};
+
+    auto merged = MergeMRRC(conf, delta);
+    CHECK(merged["distdirs"] == Expression::FromJson(R"(
+      [ {"root": "workspace", "path": "third_party"}
+      , {"root": "home", "path": ".distfiles"}
+      ]
+    )"_json));
+}
+
+TEST_CASE("local-merge") {
+    auto conf = Configuration{Expression::FromJson(R"(
+      {"just args": {"build": ["-J", "8"], "install": ["-J", "8", "--remember"]}}
+    )"_json)};
+    auto delta = Configuration{Expression::FromJson(R"(
+      {"just args": {"build": ["-J", "128"], "install-cas": ["--remember"]}}
+    )"_json)};
+
+    auto merged = MergeMRRC(conf, delta);
+    CHECK(merged["just args"] == Expression::FromJson(R"(
+      { "build": ["-J", "128"]
+      , "install-cas": ["--remember"]
+      , "install": ["-J", "8", "--remember"]
+      }
+    )"_json));
+}
