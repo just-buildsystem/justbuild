168e19a707b9b086a583df031b23aa33f0ca16ac
Klaus Aehlig <klaus.aehlig@huawei.com>
2024-06-17 10:49:42 +0200

    Add utility function for prefixing each line of a string
    ... and thus allowing proper quoting of command output.

---
+++ src/utils/cpp/prefix.hpp
@@ -0,0 +1,33 @@
+// Copyright 2024 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_SRC_UTILS_CPP_PREFIX_HPP
+#define INCLUDED_SRC_UTILS_CPP_PREFIX_HPP
+
+#include <algorithm>
+#include <sstream>
+#include <string>
+
+[[nodiscard]] static inline auto PrefixLines(const std::string& text,
+                                             const std::string& prefix = "  ")
+    -> std::string {
+    std::stringstream out{};
+    std::istringstream in{text};
+    for (std::string line{}; std::getline(in, line);) {
+        out << prefix << line << "\n";
+    }
+    return out.str();
+}
+
+#endif
---
+++ test/utils/cpp/prefix.test.cpp
@@ -0,0 +1,26 @@
+// Copyright 2024 Huawei Cloud Computing Technology Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <filesystem>
+
+#include "catch2/catch_test_macros.hpp"
+#include "src/utils/cpp/prefix.hpp"
+
+TEST_CASE("lines", "[prefix]") {
+    CHECK(PrefixLines("").empty());
+    CHECK(PrefixLines("foo\nbar\n") == "  foo\n  bar\n");
+    CHECK(PrefixLines("foo\nbar\n", ">") == ">foo\n>bar\n");
+    CHECK(PrefixLines("foo") == "  foo\n");
+    CHECK(PrefixLines("foo\nbar") == "  foo\n  bar\n");
+}
