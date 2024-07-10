f4d917600e4e87dc9ed1fa2d6140cd834d75ef25
Klaus Aehlig <klaus.aehlig@huawei.com>
2024-07-10 15:54:27 +0200

    ArtifactDigest: add order

--- src/buildtool/common/artifact_digest.hpp
+++ src/buildtool/common/artifact_digest.hpp
@@ -90,6 +90,12 @@ class ArtifactDigest {
         }
     }
 
+    [[nodiscard]] auto operator<(ArtifactDigest const& other) const -> bool {
+        return (hash_ < other.hash_) or
+               ((hash_ == other.hash_) and (static_cast<int>(is_tree_) <
+                                            static_cast<int>(other.is_tree_)));
+    }
+
   private:
     std::size_t size_{};
     std::string hash_{};
