9956e8369abf6e4735a78e30a78d010e562bea9d
Klaus Aehlig <klaus.aehlig@huawei.com>
2024-10-08 16:31:56 +0200

    tests: use --dump-plain-graph to simplify equality check

--- test/end-to-end/git-import/deduplicate.sh
+++ test/end-to-end/git-import/deduplicate.sh
@@ -20,7 +20,6 @@ readonly DEDUPLICATE="${PWD}/bin/deduplicate-tool-under-test"
 readonly GIT_IMPORT="${PWD}/bin/git-import-under-test"
 readonly JUST="${PWD}/bin/tool-under-test"
 readonly JUST_MR="${PWD}/bin/mr-tool-under-test"
-readonly ACTIONS_EQUAL="${PWD}/bin/actions-graph-equal"
 readonly LBR="${TEST_TMPDIR}/local-build-root"
 readonly OUT="${TEST_TMPDIR}/build-output"
 readonly REPO_DIRS="${TEST_TMPDIR}/repos"
@@ -102,7 +101,7 @@ cat repos-full.json
 echo
 "${JUST_MR}" -C repos-full.json --norc --just "${JUST}" \
              --local-build-root "${LBR}" analyse \
-             --dump-graph actions-full.json 2>&1
+             --dump-plain-graph actions-full.json 2>&1
 echo
 cat repos-full.json | "${DEDUPLICATE}" > repos.json
 cat repos.json
@@ -110,11 +109,11 @@ echo
 
 "${JUST_MR}" -C repos.json --norc --just "${JUST}" \
              --local-build-root "${LBR}" analyse \
-             --dump-graph actions.json 2>&1
+             --dump-plain-graph actions.json 2>&1
 
 # Verify that we reduced the number of repositories, but did
 # not change the action graph (except for the origins of the actions).
 [ $(jq -aM '.repositories | length' repos.json) -lt $(jq -aM '.repositories | length' repos-full.json) ]
-"${ACTIONS_EQUAL}" actions-full.json actions.json
+cmp actions-full.json actions.json
 
 echo "OK"
