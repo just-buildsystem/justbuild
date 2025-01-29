48c2382218a5af10177e68ba24091e77f1e045e6
Klaus Aehlig <klaus.aehlig@huawei.com>
2025-01-29 12:46:06 +0100

    garbage collection: remove garbage recursively
    When garbage collecting the oldest generation, we expect it to be
    a non-empty directory. Therefore, remove it recursively.

--- src/buildtool/storage/repository_garbage_collector.cpp
+++ src/buildtool/storage/repository_garbage_collector.cpp
@@ -58,7 +58,8 @@ auto RepositoryGarbageCollector::TriggerGarbageCollection(
             return false;
         }
         if (FileSystemManager::IsDirectory(remove_me)) {
-            if (not FileSystemManager::RemoveDirectory(remove_me)) {
+            if (not FileSystemManager::RemoveDirectory(remove_me,
+                                                       /*recursively=*/true)) {
                 Logger::Log(LogLevel::Error,
                             "Failed to remove directory {}",
                             remove_me.string());
@@ -110,7 +111,8 @@ auto RepositoryGarbageCollector::TriggerGarbageCollection(
             return false;
         }
         if (FileSystemManager::IsDirectory(remove_me)) {
-            if (not FileSystemManager::RemoveDirectory(remove_me)) {
+            if (not FileSystemManager::RemoveDirectory(remove_me,
+                                                       /*recursively=*/true)) {
                 Logger::Log(LogLevel::Error,
                             "Failed to remove directory {}",
                             remove_me.string());
