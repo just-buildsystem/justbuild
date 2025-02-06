f0a4d6a51abd1f1477fbd0b53ee14277f0ee921a
Maksim Denisov <denisov.maksim@huawei.com>
2025-02-06 11:14:33 +0100

    MessageLimits: Add header guard

--- src/buildtool/execution_api/common/message_limits.hpp
+++ src/buildtool/execution_api/common/message_limits.hpp
@@ -12,11 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef INCLUDED_SRC_BUILDTOOL_EXECUTION_API_COMMON_MESSAGE_LIMITS_HPP
+#define INCLUDED_SRC_BUILDTOOL_EXECUTION_API_COMMON_MESSAGE_LIMITS_HPP
+
 #include <cstddef>
 
 #include <grpc/grpc.h>
 
 // Max size for batch transfers
-static constexpr std::size_t kMaxBatchTransferSize = 3UL * 1024 * 1024;
+inline constexpr std::size_t kMaxBatchTransferSize = 3UL * 1024 * 1024;
 static_assert(kMaxBatchTransferSize < GRPC_DEFAULT_MAX_RECV_MESSAGE_LENGTH,
               "Max batch transfer size too large.");
+
+#endif  // INCLUDED_SRC_BUILDTOOL_EXECUTION_API_COMMON_MESSAGE_LIMITS_HPP
