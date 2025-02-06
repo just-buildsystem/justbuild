e67f08133d54d1b51f00f22540917e56825e88de
Maksim Denisov <denisov.maksim@huawei.com>
2025-02-06 12:17:22 +0100

    MessageLimits: move grpc to private deps.

--- src/buildtool/execution_api/common/message_limits.cpp
+++ src/buildtool/execution_api/common/message_limits.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Huawei Cloud Computing Technology Co., Ltd.
+// Copyright 2025 Huawei Cloud Computing Technology Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,12 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef INCLUDED_SRC_BUILDTOOL_EXECUTION_API_COMMON_MESSAGE_LIMITS_HPP
-#define INCLUDED_SRC_BUILDTOOL_EXECUTION_API_COMMON_MESSAGE_LIMITS_HPP
+#include "src/buildtool/execution_api/common/message_limits.hpp"
 
-#include <cstddef>
+#include <grpc/grpc.h>
 
-// Max size for batch transfers
-inline constexpr std::size_t kMaxBatchTransferSize = 3UL * 1024 * 1024;
-
-#endif  // INCLUDED_SRC_BUILDTOOL_EXECUTION_API_COMMON_MESSAGE_LIMITS_HPP
+static_assert(kMaxBatchTransferSize < GRPC_DEFAULT_MAX_RECV_MESSAGE_LENGTH,
+              "Max batch transfer size too large.");
