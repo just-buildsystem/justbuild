d45e66d14604c5d75a0ae4ee697f3b0ae3e16701
Klaus Aehlig <klaus.aehlig@huawei.com>
2025-03-06 11:15:58 +0100

    Start 1.6 development cycle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Release `1.6.0` (UNRELEASED)
+
+A feature release on top of `1.5.0`, backwards compatible.
+
 ## Release `1.5.0` (2025-03-06)
 
 A feature release on top of `1.4.0`, backwards compatible.
--- src/buildtool/main/version.cpp
+++ src/buildtool/main/version.cpp
@@ -23,9 +23,9 @@
 
 auto version() -> std::string {
     static const std::size_t kMajor = 1;
-    static const std::size_t kMinor = 5;
+    static const std::size_t kMinor = 6;
     static const std::size_t kRevision = 0;
-    std::string suffix = std::string{};
+    std::string suffix = "~alpha";
 #ifdef VERSION_EXTRA_SUFFIX
     suffix += VERSION_EXTRA_SUFFIX;
 #endif
