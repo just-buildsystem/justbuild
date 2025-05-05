just-buildsystem/justbuild

commit 5e104a526cf76fe75312d2fd288a3c88f506fb0a

"generic" rule: verify staging conflicts on inputs
The "generic" rules deliberately resolves conflicts on identical
paths in a latest-wins fashion (seeing all artifacts as later than
all runfiles) to allow an easy way to define actions. However, the
inputs stage obtained by this resolution can still contain conflicts
and those are an error. Properly detect those. Also clarify in the
documentation, that only conflicts on identical paths are resolved
in the described priority, not semantic overlap.

src/buildtool/build_engine/target_map/built_in_rules.cpp
@@ -1369,8 +1369,17 @@ void GenericRuleWithDeps(
     for (auto const& dep : dependency_values) {
         inputs = ExpressionPtr{Expression::map_t{inputs, (*dep)->RunFiles()}};
     }
+    auto inputs_conflict = BuildMaps::Target::Utils::tree_conflict(inputs);
     for (auto const& dep : dependency_values) {
         inputs = ExpressionPtr{Expression::map_t{inputs, (*dep)->Artifacts()}};
+    }
+    // While syntactical conflicts are resolved in a latest wins (with artifacts
+    // after runfiles), semantic path conclicts are an error.
+    if (inputs_conflict) {
+        (*logger)(fmt::format("Input artifacts have staging conflict on {}",
+                              nlohmann::json(*inputs_conflict).dump()),
+                  /*fatal=*/true);
+        return;
     }
     std::vector<Tree::Ptr> trees{};
     inputs = BuildMaps::Target::Utils::add_dir_for(

commit f249a220913f4421c58c9c6251e4f3dce67e251c

generic rule: fix conflict check
For a generic rule, it is an error if map union of various
inputs (overlayed in correct order) does not form a proper stage. To
implement this check properly, we first have to construct the map of
all inputs and only then perform the staging check and not do the check
with only the runfiles, as 5e104a526cf76fe75312d2fd288a3c88f506fb0a
accidentally did. Fix this.

src/buildtool/build_engine/target_map/built_in_rules.cpp
@@ -1455,10 +1455,10 @@ void GenericRuleWithDeps(
     for (auto const& dep : dependency_values) {
         inputs = ExpressionPtr{Expression::map_t{inputs, (*dep)->RunFiles()}};
     }
-    auto inputs_conflict = BuildMaps::Target::Utils::tree_conflict(inputs);
     for (auto const& dep : dependency_values) {
         inputs = ExpressionPtr{Expression::map_t{inputs, (*dep)->Artifacts()}};
     }
+    auto inputs_conflict = BuildMaps::Target::Utils::tree_conflict(inputs);
     // While syntactical conflicts are resolved in a latest wins (with artifacts
     // after runfiles), semantic path conclicts are an error.
     if (inputs_conflict) {